468363b0acdad6a2cfbb55c260954f00b0151453
dhirendra <dhirendra.kumar@agoda.com>
2020-01-25 16:36:17 +0700

    add download method

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 const express = require('express');
  
+
+ var stream = require('stream');
+//...
+app.get('/download', function(request, response){
+  //...
+  var fileContents = Buffer.from(fileData, "base64");
+
+  var readStream = new stream.PassThrough();
+  readStream.end(fileContents);
+
+  response.set('Content-disposition', 'attachment; filename=' + fileName);
+  response.set('Content-Type', 'text/plain');
+
+  readStream.pipe(response);
+});
 const app = express();
  
 app.get('/', (req, res) => {
